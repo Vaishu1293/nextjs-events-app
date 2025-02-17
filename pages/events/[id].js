a299d8cc131e13a9f62d114b053d5ad352cb9e53
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-17 09:00:13 +0000

    task

---
+++ pages/events/[id].js
@@ -0,0 +1,5 @@
+export default function EventsDetailsPage(){
+    return (<div>
+        <h1>The Events Detail Page.</h1>
+    </div>);
+}
---
+++ pages/events/page.js
@@ -0,0 +1,5 @@
+export default function EventsPage(){
+    return (<div>
+        <h1>The Events Page.</h1>
+    </div>);
+}
