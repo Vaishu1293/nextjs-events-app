17df15db58caff15983df08fe2daa33839c6b368
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-17 18:25:07 +0000

    filter

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,13 @@
+import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/events-search';
+import {getAllEvents} from '../../dummy-data';
+
 export default function AllEventsPage(){
-    return (<div>
-        <h1>All Events</h1>
-    </div>);
+    const events = getAllEvents();
+    return (<>
+        <div>
+            <EventSearch />
+            <EventList items={events} />
+        </div>
+    </>);
 }
