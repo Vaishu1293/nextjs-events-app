cc14dd19bddc87368167baf8d86a2650dd259592
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-17 17:44:19 +0000

    event-detail-page

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,5 +1,28 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 export default function EventsDetailPage(){
-    return (<div>
-        <h1>Event Detail</h1>
-    </div>);
+    const router = useRouter();
+    console.log(router.query);
+    const eventId = router.query.eventid;
+    const event = getEventById(eventId);
+    if(!event){
+        return <p>No event found!</p>;
+    }
+    return (<Fragment>
+        <EventSummary title={event.title}/>
+        <EventLogistics 
+        date={event.date} 
+        address={event.location} 
+        image={event.image} 
+        imageAlt={event.title} 
+        />
+        <EventContent>
+            <p>{event.description}</p>
+        </EventContent>
+    </Fragment>);
 }
