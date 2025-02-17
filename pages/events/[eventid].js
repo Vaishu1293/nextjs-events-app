00dc9df4c5cb1a28cd7b784ccfc68afc972ee8ca
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-17 17:26:47 +0000

    icons-buttons

---
+++ pages/events/[eventid].js
@@ -0,0 +1,5 @@
+export default function EventsDetailPage(){
+    return (<div>
+        <h1>Event Detail</h1>
+    </div>);
+}
--- pages/events/[id].js
+++
@@ -1,5 +0,0 @@
-export default function EventsDetailsPage(){
-    return (<div>
-        <h1>The Events Detail Page.</h1>
-    </div>);
-}
